c6475feaa4398ea64a71ada617a4196bb9f7d7cb
Andriy <andriy.av95@ukr.net>
2018-11-29 22:31:58 +0200

    список записів з'являється при вході в додаток

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClientService} from '../Services/HttpClientService';
+import { HttpClientService } from '../Services/HttpClientService';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-general',
@@ -9,11 +10,16 @@ import {HttpClientService} from '../Services/HttpClientService';
 
 export class GeneralComponent implements OnInit {
   category: Category[]  = [];
-  constructor(private http: HttpClientService) {}
+  constructor(private http: HttpClientService,
+              private router: Router,
+              private ar: ActivatedRoute) {}
 
   ngOnInit() {
     this.http.httpGet().subscribe((data: Category[]) => {
       this.category = data;
+      if (this.ar.children.length === 0) {
+        this.router.navigateByUrl(`home/${this.category[0].category}`);
+      }
     });
   }
 }
